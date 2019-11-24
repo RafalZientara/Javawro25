2ec151c7d7e7f06a3b546b3f67b6829a22c65f42
Rafal Zientara <rafal.zientara92@gmail.com>
2019-11-24 14:16:52 +0100

    Vector paint layout + main

---
+++ src/main/java/pl/sda/rafal/zientara/game/lesson4/paint/MainPaintJavaFx.java
@@ -0,0 +1,23 @@
+package pl.sda.rafal.zientara.game.lesson4.paint;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainPaintJavaFx extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass()
+                .getResource("/paint.fxml"));
+        primaryStage.setTitle("Vector paint");
+        primaryStage.setScene(new Scene(root, primaryStage.getWidth(),
+                primaryStage.getHeight()));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
